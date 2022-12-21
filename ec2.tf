5f084204112787da1dde8676cf4f3136e65c96e1
atabonglefac <atabsmagel@gmail.com>
2022-12-20 19:54:04 -0500

    creating ec2 instance

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+   region = "us-east-1"
+}
+resource "aws_instance" "terraform_demo" {
+    ami = "ami-09d3b3274b6c5d4aa"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "kubs"
+        Env = "dev"
+    }
+}     
+
+
+
+
